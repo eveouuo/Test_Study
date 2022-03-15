6d971794352404cd3d1c0ccc8e5f65bd714aa87a
eve <92919741+seoyeonAn@users.noreply.github.com>
2022-03-16 01:09:28 +0900

    220315 commit

--- A-3/js/common.js
+++ A-3/js/common.js
@@ -1,3 +1,15 @@
-$(document).raedy(function(){
+$(document).ready(function(){
+    $("header ul.gnb > li").hover(function(){
+        $(this).find("ul.sub").stop().slideDown();
+    },function(){
+        $(this).find("ul.sub").stop().slideUp();
+    });
 
+
+
+    $("#contents .noti_gal h2").click(function(){
+        $("#contents .noti_gal h2,#contents .noti_gal ul").removeClass("on");
+        $(this).addClass("on");
+        $(this).next("ul").addClass("on");
+    });
 });
