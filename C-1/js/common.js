fb03c7422e8f5ab959d29d07d732a7ceabebff15
eve <lovelylove.eve@gmail.com>
2022-03-18 18:01:18 +0900

    220318 commit

--- C-1/js/common.js
+++ C-1/js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    $("nav.gnb > ul > li").hover(function(){
+        $(this).find("ul.sub").stop().slideDown();
+    },function(){
+        $(this).find("ul.sub").stop().slideUp();
+    });
 
+    var currunt = 0;
+    setInterval(function(){
+        var next = (currunt + 1) % 3;
+
+        $(".slideItem").eq(currunt).fadeOut();
+        $(".slideItem").eq(next).fadeIn();
+
+        currunt = next;
+    },3000);
+
+    
 });
